027a72c526b8d4f0cf0ce9679acaa2dd6182b0d7
Aluno <aluno@etec.sp.gov.br>
2023-10-17 22:48:38 -0300

    Aula 17_10_2023 Switch

---
+++ PA/Aula_17_10_2023/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Aula_17_10_2023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float num1, num2, resultado;
+            string operador;
+
+            Console.Write("Informe o 1° Número: ");
+            num1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Informe o 2° Número: ");
+            num2 = float.Parse(Console.ReadLine());
+
+            Console.Write("Informe o operador: ");
+            operador = Console.ReadLine();
+
+            switch (operador)
+            {
+                case "+":
+                    resultado = num1 + num2;
+                    Console.WriteLine("O resultado é: " + resultado);
+                    break;
+                case "-":
+                    resultado = num1 - num2;
+                    Console.WriteLine("O resultado é: " + resultado);
+                    break;
+                case "*":
+                    resultado = num1 * num2;
+                    Console.WriteLine("O resultado é: " + resultado);
+                    break;
+                case "/":
+                    resultado = num1 / num2;
+                    Console.WriteLine("O resultado é:" + resultado);
+                    break;
+                default:
+                    Console.WriteLine("Operador Invalido");
+                    break;
+            }
+        }
+    }
+}
