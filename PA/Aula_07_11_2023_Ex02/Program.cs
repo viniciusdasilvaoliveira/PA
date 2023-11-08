ce11b1e412ff0885332bb658683ca1ab4e551c29
Aluno <aluno@etec.sp.gov.br>
2023-11-07 22:46:47 -0300

    Aula_07_11_2023_Exercicios_FOR

---
+++ PA/Aula_07_11_2023_Ex02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Aula_07_11_2023_Ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fator;
+
+            Console.WriteLine("Informe o Fator Desejado: ");
+            fator = int.Parse(Console.ReadLine());
+           
+
+            for (int contador = 1; contador <= 10; contador++)
+            {
+                Console.WriteLine($"{fator} x {contador} = {fator * contador}");
+            }
+        }
+    }
+}
---
+++ PA/Aula_07_11_2023_Ex03/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Aula_07_11_2023_Ex03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.Write("Informe um número: ");
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero % 2 == 0)
+           
+                Console.WriteLine($"O número {numero} é PAR!");
+            else
+                Console.WriteLine($"O número {numero} é IMPAR!");
+        }
+    }
+}
