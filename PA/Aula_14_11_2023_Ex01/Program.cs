d1dc059cdb8055d713bd44cb6254c54f67092795
Aluno <aluno@etec.sp.gov.br>
2023-11-14 22:46:02 -0300

    Aula_14_11_2023_ Exercicios

---
+++ PA/Aula_14_11_2023_Ex01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Aula_14_11_2023_Ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("=========");
+            Console.WriteLine("SUBTRAÇÃO");
+            Console.WriteLine("=========");
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            for (int contador = 1; contador <= 3; contador++)
+            {
+                Console.Write("Informe o primeiro número: ");
+                n1 = int.Parse(Console.ReadLine());
+
+                Console.Write("Informe o segundo número: ");
+                n2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"{n1} - {n2} = {n1 - n2 }");
+            }
+        }
+    }
+}
---
+++ PA/Aula_14_11_2023_Ex02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Aula_14_11_2023_Ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("---------------");
+            Console.WriteLine("NÚMEROS IMPARES");
+            Console.WriteLine("---------------");
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Write("Informe a quantidade de números: ");
+            numero = int.Parse(Console.ReadLine());
+
+            for (int contador = 1; contador  <= numero; contador ++)
+            {
+                if (contador % 2 == 1)
+                    Console.WriteLine($"Numeros Impares: {contador}");
+            }
+        }
+    }
+}
