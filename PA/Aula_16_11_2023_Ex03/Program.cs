7626d83c4536df8f31302287a7c9fb4f2b80230a
Aluno <aluno@etec.sp.gov.br>
2023-11-16 22:46:46 -0300

    Aula_16_11_2023

---
+++ PA/Aula_16_11_2023_Ex03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Aula_16_11_2023_Ex03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade, contador = 0, somaIdade = 0;
+
+            do
+            {
+                Console.Write("Informe as idades para obter a media:");
+                idade = int.Parse(Console.ReadLine());
+                contador++;
+                if (idade == 0)
+                    break;
+                somaIdade += idade;
+
+            } while (idade != 0);
+            {
+                Console.Write($"A media das idades: {somaIdade / contador}");
+            }
+
+        }
+    }
+}
