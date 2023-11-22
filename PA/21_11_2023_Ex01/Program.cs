642381c2be12c5354f389d6ac870692295d3325e
Aluno <aluno@etec.sp.gov.br>
2023-11-21 22:43:46 -0300

    Aula_21_11_2023_Exercicíos_01_e_02

---
+++ PA/21_11_2023_Ex01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _21_11_2023_Ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float altura, maiorAltura = 0, menorAltura = 0;
+
+            for (int contador = 1; contador <=15; contador++)
+            {
+                Console.Write($"Informe a Altura da {contador}° Pessoa: ");
+                altura = float.Parse(Console.ReadLine());
+
+
+                if (altura >= maiorAltura)
+                    maiorAltura = altura;
+
+                if (altura <= menorAltura || menorAltura == 0)
+                    menorAltura = altura;
+
+            }
+                Console.WriteLine($"MAIOR ALTURA: {maiorAltura}");
+                Console.WriteLine($"MENOR ALTURA: {menorAltura}");
+        }
+    }
+}
